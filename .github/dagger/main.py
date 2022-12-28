8b7e5836e67b1bfbf4e7853af44ae13ee257a51b
GonzalezAndrew <andrewgonzo23@gmail.com>
2022-12-28 12:41:58 -0600

    testing dagger building and pushing a docker image to ttl.sh

--- .github/dagger/main.py
+++ .github/dagger/main.py
@@ -3,9 +3,7 @@ Run tests for multiple Python versions concurrently.
 """
 
 import sys
-
 import anyio
-
 import dagger
 
 
@@ -13,7 +11,7 @@ async def test():
     versions = ["3.7", "3.8", "3.9", "3.10", "3.11"]
 
     async with dagger.Connection(dagger.Config(log_output=sys.stderr)) as client:
-        
+
         async def test_version(version: str):
             python = (
                 client.container().from_(f"python:{version}-slim-buster")
@@ -36,5 +34,14 @@ async def test():
     print("All tasks have finished")
 
 
+async def build():
+    async with dagger.Connection(dagger.Config(log_output=sys.stderr)) as client:
+        python = client.container().build(client.host().directory("../../service_1/"))
+
+        image = await python.publish("ttl.sh/footestdagger:5m")
+
+    print(f"Image has been pushed {image}")
+
+
 if __name__ == "__main__":
-    anyio.run(test)
+    anyio.run(build)
